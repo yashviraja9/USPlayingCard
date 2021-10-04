1b33695be68e6ce2fd4b6082d7a0fb27ae02140a
yashviraja9 <YR6@myscc.ca>
2021-10-04 12:15:50 -0400

    Add project files.

---
+++ USPlayingCard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace USPlayingCard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
