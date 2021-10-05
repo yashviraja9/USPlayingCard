416b7e6953ee1216102a1d88d62e28c290756cdd
Yashvi Brijmohan Raja <W0766432@myscc.ca>
2021-10-05 12:42:32 -0400

    Part 6 - controller methods and views

--- USPlayingCard/Models/Card.cs
+++ USPlayingCard/Models/Card.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace USPlayingCard.Models
 {
@@ -12,6 +13,8 @@ namespace USPlayingCard.Models
         public string Title { get; set; }
         public string Brand { get; set; }
         public string Material { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
     }
 }
