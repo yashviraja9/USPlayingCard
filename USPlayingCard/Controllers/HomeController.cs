8756a1afbaafa9c9e0028aeb5d7a069e2c1319fb
Yashvi Brijmohan Raja <W0766432@myscc.ca>
2021-10-06 16:30:47 -0400

    add about method in HomeController

--- USPlayingCard/Controllers/HomeController.cs
+++ USPlayingCard/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace USPlayingCard.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
